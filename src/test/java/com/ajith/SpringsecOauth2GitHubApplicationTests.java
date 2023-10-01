1c62518ddb0965c9de08dded0e989319f06628cb
Ajith Kumar <ajithpoojary1798@gmail.com>
2023-10-01 14:32:05 +0530

    Simple Oauth2 implementation using GitHub as a authentication Server

---
+++ src/test/java/com/ajith/SpringsecOauth2GitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecOauth2GitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
